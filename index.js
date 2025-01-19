a4e2f64f430696cdc3b2d004cd83d0cccd11a0af
unknown <alazim-star>
2025-01-19 11:10:53 +0600

    second commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app =express()
 const cors = require('cors');
 const jwt=require('jsonwebtoken')
 require('dotenv').config()
-// const stripe=require('stripe')(process.env.STRIPE_SECRET_KEY)
+const stripe=require('stripe')(process.env.STRIPE_SECRET_KEY)
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port=process.env.PORT || 5000
@@ -38,6 +38,7 @@ const scholarshipCollection =client.db('scholarshipDB').collection('allScholarsh
 const reviewCollection =client.db('scholarshipDB').collection('reviews')
 const userCollection = client.db("scholarshipDB").collection("users")
 const applicationCollection = client.db("scholarshipDB").collection("applications")
+const paymentCollection = client.db("scholarshipDB").collection("payments")
 
 
 //middleware 
@@ -67,24 +68,6 @@ app.post('/jwt',async(req,res)=>{
 
 })
 
-// app.post('/jwt', async(req,res)=>{
-//   const user=req.body
-//   const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'12h'})
-
-//   console.log(token);
-//   res
-//   .cookie ('token',token,{
-//     httpOnly: true,
-//                 secure: process.env.NODE_ENV === 'production', 
-//                 sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-//   })
-//   .send({success:true})
-// })
-
-
-// Auth related APIs jwt token  logout 
-
-
 // use verify admin after verify token 
 const verifyAdmin= async(req,res,next)=>{
   const email=req.decoded.email
@@ -98,17 +81,56 @@ const verifyAdmin= async(req,res,next)=>{
   next()
 
 }
+// use verify admin after verify token 
+const verifyModerator= async(req,res,next)=>{
+  const email=req.decoded.email
+  const query={email: email}
+  const user=await userCollection.findOne(query)
+  const isModerator=user?.role === 'moderator'
+  if (!isModerator) {
+    return res.status(403).send({message: 'forbidden access'})
+    
+  }
+  next()
+
+}
 
 
 
 //for user related Api 
 
 
-app.get ('/users',verifyToken,verifyAdmin,async(req,res)=>{
+app.get ('/users',async(req,res)=>{
   // console.log(req.headers);
   const result=await userCollection.find().toArray()
   res.send(result)
 })
+
+app.get('/users/:email', async (req, res) => {
+  const email = req.params.email;
+  console.log({ email });
+    const query = { email: email };
+    const result = await userCollection.findOne(query);   
+      res.send(result);  
+
+});
+
+// /user er email store korar jonno 
+app.post('/users',async(req,res)=>{
+  const user=req.body
+  // jodi email akta take user er tahole abar sei email save hobe na 
+  // insert email if use doesnot exists:
+  //you can do this in many ways (1,email unique 2. upsert 3.simple checking)
+  const query={email:user.email}
+  const existingUser=await userCollection.findOne(query)
+  if (existingUser) {
+    return res.send({message : 'user already exists',insertedId:null})
+    
+  }
+  const result =await userCollection.insertOne(user)
+  res.send(result)
+})
+
 
 //email er modde teke admin email konta bar korar jonno
 app.get('/users/admin/:email', verifyToken, async (req, res) => {
@@ -131,29 +153,38 @@ app.get('/users/admin/:email', verifyToken, async (req, res) => {
 });
 
 
+//admin roll set korar jonno 
+app.patch('/users/admin/:id',async(req,res)=>{
+  const id=req.params.id
+  const filter={_id: new ObjectId(id)}
+  const updatedDoc={
+    $set:{
+      role: 'admin'
 
-
-
-
-
-//user er email store korar jonno 
-app.post('/users',async(req,res)=>{
-  const user=req.body
-  // jodi email akta take user er tahole abar sei email save hobe na 
-  // insert email if use doesnot exists:
-  //you can do this in many ways (1,email unique 2. upsert 3.simple checking)
-  const query={email:user.email}
-  const existingUser=await userCollection.findOne(query)
-  if (existingUser) {
-    return res.send({message : 'user already exists',insertedId:null})
-    
+    }
   }
-  const result =await userCollection.insertOne(user)
+  const result=await userCollection.updateOne(filter,updatedDoc)
   res.send(result)
 })
 
+//admin roll set korar jonno 
+app.patch('/users/moderator/:id',async(req,res)=>{
+  const id=req.params.id
+  const filter={_id: new ObjectId(id)}
+  const updatedDoc={
+    $set:{
+      role: 'moderator'
+
+    }
+  }
+  const result=await userCollection.updateOne(filter,updatedDoc)
+  res.send(result)
+})
+
+
+
 //user delet korar jonno
-app.delete('/users/:id',verifyToken,verifyAdmin,async(req,res) => {
+app.delete('/users/:id',async(req,res) => {
   const id =req.params.id
   const query={_id: new ObjectId(id)}
   const result=await userCollection.deleteOne(query)
@@ -163,12 +194,24 @@ app.delete('/users/:id',verifyToken,verifyAdmin,async(req,res) => {
 })
 
 
+
+
 // / for all reviews related api
 app.get('/reviews',async(req,res)=>{
     const result=await reviewCollection.find().toArray()
     res.send(result)
 })
 
+app.get("/reviews/:email",async (req, res) => {
+  const email = req.params.email; 
+  console.log({email});
+  const query={email:email}
+
+    const result = await reviewCollection.find( query ).toArray();
+    res.send(result);
+    // console.log(result);
+  
+});
 
 
 app.get('/reviews/:id', async (req, res) => {
@@ -177,6 +220,29 @@ const query={_id:new ObjectId(id)}
 const result=await reviewCollection.findOne(query)
 res.send(result)
 });
+
+
+
+app.put('/reviews/:id', async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) }; 
+  const options = { upsert: true }; 
+  const scholarship = req.body; 
+
+  // Define the update operation
+  const updatedScholarship = {
+    $set: {
+      comment: scholarship.comment,
+      rating: scholarship.rating,
+     
+    },
+  };
+
+  const result = await reviewCollection.updateOne(filter, updatedScholarship, options);
+
+  res.send(result);  
+});
+
 
 
 
@@ -193,6 +259,13 @@ res.send(result)
 
 
 
+app.delete('/reviews/:id',async(req,res)=>{
+  const id=req.params.id 
+  const query={_id:new ObjectId(id)}
+  const result =await reviewCollection.deleteOne(query)
+  res.send(result)
+
+})
 
 /// for read using get 
 app.get('/allScholarship',async(req,res)=>{
@@ -203,12 +276,81 @@ app.get('/allScholarship',async(req,res)=>{
 
 
 // for give data in server use post 
-app.post('/allScholarship', async (req,res)=>{
+app.post('/allScholarship',verifyToken,async (req,res)=>{
     const newScholarship=req.body
     console.log(newScholarship);
     const result=await scholarshipCollection.insertOne(newScholarship)
     res.send(result)
 })
+app.delete('/allScholarship/:id',async(req,res)=>{
+  const id=req.params.id 
+  const query={_id:new ObjectId(id)}
+  const result =await scholarshipCollection.deleteOne(query)
+  res.send(result)
+
+})
+
+
+app.put('/allScholarship/:id', async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) }; 
+  const options = { upsert: true }; 
+  const scholarship = req.body; 
+
+  // Define the update operation
+  const updatedScholarship = {
+    $set: {
+      scholarshipName: scholarship.scholarshipName,
+      universityName: scholarship.universityName,
+      universityLogo: scholarship.universityLogo, 
+      universityCountry: scholarship.universityCountry,
+      universityCity: scholarship.universityCity,
+      universityWorldRank: scholarship.universityWorldRank,
+      subjectCategory: scholarship.subjectCategory, 
+      scholarshipCategory: scholarship.scholarshipCategory, 
+      degree: scholarship.degree, 
+      tuitionFees: scholarship.tuitionFees || null, 
+      applicationFees: scholarship.applicationFees,
+      serviceCharge: scholarship.serviceCharge,
+      applicationDeadline: scholarship.applicationDeadline,
+      scholarshipPostDate: scholarship.scholarshipPostDate,
+      postedUserEmail: scholarship.postedUserEmail,
+    },
+  };
+
+  const result = await scholarshipCollection.updateOne(filter, updatedScholarship, options);
+
+  res.send(result);  
+});
+
+app.put('/applications/:id', async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) }; 
+
+  const applying = req.body;
+
+  const updateFields = {
+    $set: { 
+      ...applying, 
+    },
+  };
+
+  try {
+   
+    const result = await applicationCollection.updateOne(filter, updateFields);
+    if (result.matchedCount === 0) {
+      return res.status(404).send({ message: "Application not found." });
+    }
+    res.send(result);
+  } catch (error) {
+    
+    console.error(error);
+    res.status(500).send({ message: "Error updating the application." });
+  }
+});
+
+
+
 
 
 // application related api 
@@ -233,9 +375,6 @@ app.get("/applications/:email",async (req, res) => {
     const email = req.params.email; 
     console.log({email});
     const query={email:email}
-
-    console.log('cookies for cookies',req.cookies);
-    
       const result = await applicationCollection.find( query ).toArray();
       res.send(result);
       // console.log(result);
@@ -272,30 +411,47 @@ app.get('/applications/:id',async(req,res)=>{
   res.send(result)
 })
 
+// / payment related method intent 
+app.post('/create-payment-intent',async(req,res)=>{
+  const {applicationFees}=req.body
+  const amount=parseInt(applicationFees * 100)
+  console.log(amount,'amount inside the intent');
+  const PaymentIntent=await stripe.paymentIntents.create({
+    amount:amount,
+    currency:'usd',
+    payment_method_types:['card']
 
-app.put('/applications/:id', async (req, res) => {
-    const id = req.params.id;
-    const filter = { _id: new ObjectId(id) }; 
-    const options = { upsert: true }; 
-    const applying = req.body; 
-  
+  })
+  res.send({
+    clientSecret:PaymentIntent.client_secret
+  })
+})
 
-    const applyings = {
-      $set: {
-    
-  
-       applyingDate:applying.applyingDate
-       
-       
-      },
+
+
+// payment related api 
+app.post('/payments', async (req, res) => {
+  const payment = req.body;
+
+  try {
+    const paymentResult = await paymentCollection.insertOne(payment);
+    console.log('Payment info:', payment);
+
+    const query = {
+      _id: {
+        $in: payment.applicationId.map(id => new ObjectId(id))
+      }
     };
-  
-  
-    const result = await applicationCollection.updateOne(filter,applyings,options);
-  
-  
-    res.send(result);
-  });
+
+    const deleteResult = await paymentCollection.deleteMany(query);
+
+    // Ensure only one response object is sent
+    res.send({ paymentResult, deleteResult });
+  } catch (error) {
+    console.error('Error processing payment:', error);
+    res.status(500).send({ error: 'Payment processing failed.' });
+  }
+});
 
 
 
